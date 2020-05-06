bdbdbc538fd7147b60a9d4dc5cbcb4169419be4b
Sara <saritazg2010@gmail.com>
2020-05-06 16:50:05 -0600

    Now it is done

--- ejemplos_redux/src/sagas/owners.js
+++ ejemplos_redux/src/sagas/owners.js
@@ -39,6 +39,84 @@ function* fetchOwners(action){
 
 
 }
+function* addOwner(action) {
+    const { oldId, petOwner } = action.payload
+    try {
+       
+        const isAuthenticated = yield select(selectors.isAuthenticated);
+        if (isAuthenticated) {
+            const token = yield select(selectors.getAuthToken);
+            const response = yield call(
+                fetch,
+                `${API_BASE_URL}`,
+                    {
+                    method: 'POST',
+                    body: JSON.stringify({ name: petOwner }),
+                    headers:{
+                        'Content-Type': 'application/json',
+                        'Authorization': `JWT ${token}`,
+                    },
+                }
+            );
+            if (response.status < 400) {
+                const { id, name } = yield response.json();
+                yield put(actions.completeAddingPetOwner(oldId, { id, name }));
+            } else {
+                yield put(actions.failAddingPetOwner(oldId, "Fallo horrible la conexin mano"));
+            }
+        } else {
+            yield put(actions.failAddingPetOwner(oldId, "You are not logged in"));
+        }
+    } catch (e) {
+        yield put(actions.failAddingPetOwner(oldId, e.message));
+    }
+}
+
+function* removeOwner(action) {  
+    const { id } = action.payload 
+    try {
+        const isAuthenticated = yield select(selectors.isAuthenticated);
+        if (isAuthenticated) {
+        
+            const token = yield select(selectors.getAuthToken);
+            const response = yield call(
+                fetch,
+                `${API_BASE_URL}${id}`,
+                    {
+                    method: 'DELETE',
+                    headers:{
+                        'Content-Type': 'application/json',
+                        'Authorization': `JWT ${token}`,
+                    },
+                }
+            );
+            if (response.status < 400) {
+                yield put(actions.completeRemovingPetOwner());
+            } else {
+                yield put(actions.failRemovingPetOwner(id, "Fallo horrible la conexion mano"));
+            }
+        } else {
+            yield put(actions.failRemovingPetOwner(id, "You are not logged int"));
+        }
+    } catch (e) {
+        console.log(e)
+        yield put(actions.failRemovingPetOwner(id, e.message));
+    }
+}
+
+export function* watchAddOwner() {
+    yield takeEvery(
+        types.PET_OWNER_ADD_STARTED,
+        addOwner,
+    );
+} 
+
+export function* watchRemoveOwner() {
+    yield takeEvery(
+        types.PET_OWNER_REMOVE_STARTED,
+        removeOwner,
+    );
+}
 
 export function* watchFetchOwners(){
     yield takeEvery(types.PET_OWNERS_FETCH_STARTED, fetchOwners);
