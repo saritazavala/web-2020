39b47728058ed037fb31bb70b42cd043ff6afa81
Sara Zavala <saritazg2010@gmail.com>
2020-05-06 13:05:52 -0600

    changes in sagas

--- ejemplos_django/example/owners/views.py
+++ ejemplos_django/example/owners/views.py
@@ -12,25 +12,4 @@ from owners.serializers import OwnerSerializer
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
-    permission_classes = (
-    APIPermissionClassFactory(
-        name = 'OwnerPermission',
-        permission_configuration = {
-            'base': {                     
-            'create': True,                     
-            'list': True,                     
-
-            },
-
-            'instance': { 
-                'retrieve': True,                    
-                'destroy': True,                    
-                'update': True,                    
-                'partial_update': True, 
-                'events': True, 
-                } 
-
-        }
-    )
-
-    )
+ 
